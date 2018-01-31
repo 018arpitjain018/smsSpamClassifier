505be873fb86d9f83e32841566c1fdbddfb95b7f
Arpit Jain <018arpitjain018@gmail.com>
2018-01-31 10:46:07 +0530

    spamClassifier project uploading github.

---
+++ spamDetector.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv('dataset/SMSSpamCollection.tsv', index_col=0, sep='\t')
+
+hv = HashingVectorizer(n_features=20)
+features = hv.transform(list(df["message"])).toarray()
+labels = list(df["category"])
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=.1)
+
+clf = GaussianNB()
+
+clf.fit(features_train, labels_train)
+
+prediction = clf.predict(features_test)
+
+print("Test case 1:")
+print("Extected label: ", labels_test[1])
+print("Predicted label: ", prediction[1])
+
+print("\nTest Case 2:")
+print("Extected label: ", labels_test[165])
+print("Predicted label: ", prediction[187])
+
+print("\nAccuracy Score: ", accuracy_score(labels_test, prediction))
+
+
+
+
+
+
